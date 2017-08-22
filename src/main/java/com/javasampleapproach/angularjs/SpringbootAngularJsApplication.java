fedb197d4d8a3649f3c738351271a2531aea2323
Priyanka Baddam <pbaddam@opentext.com>
2017-08-22 11:47:14 +0530

    adding files

---
+++ src/main/java/com/javasampleapproach/angularjs/SpringbootAngularJsApplication.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.angularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularJsApplication.class, args);
+	}
+}
